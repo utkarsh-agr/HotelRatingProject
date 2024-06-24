a4d0dfe3c1fb27f86586090818d8644d90a364dd
Utkarsh Agrawal <agrawalutkarsh1906@gmail.com>
2024-06-24 15:02:54 +0530

    Now my internal services are contacting other services via gateway

--- UserService/src/main/java/com/userservice/externalServices/ApiGatewayService.java
+++ UserService/src/main/java/com/userservice/externalServices/ApiGatewayService.java
@@ -1,12 +1,20 @@
 package com.userservice.externalServices;
 
+
 import com.userservice.payloads.Hotel;
+import com.userservice.payloads.Rating.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("HOTEL-SERVICE")
-public interface HotelService {
+import java.util.List;
+
+@FeignClient("API-GATEWAY")
+public interface ApiGatewayService {
+
+    @GetMapping("/ratings/user/{userId}")
+    ResponseEntity<List<Rating>> getRatingsOfUser(@PathVariable("userId") String userId);
 
     @GetMapping("/hotels/{hotelId}")
     Hotel getHotel(@PathVariable("hotelId") String hotelId);
--- UserService/src/main/java/com/userservice/services/impl/UserServicesImpl.java
+++ UserService/src/main/java/com/userservice/services/impl/UserServicesImpl.java
@@ -2,23 +2,17 @@ package com.userservice.services.impl;
 
 import com.userservice.entities.User;
 import com.userservice.exceptions.ResourceNotFoundException;
-import com.userservice.externalServices.HotelService;
-import com.userservice.externalServices.RatingService;
+import com.userservice.externalServices.ApiGatewayService;
 import com.userservice.payloads.Hotel;
 import com.userservice.payloads.Rating.Rating;
 import com.userservice.repositories.UserRepository;
 import com.userservice.services.UserServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServicesImpl implements UserServices {
@@ -30,10 +24,7 @@ public class UserServicesImpl implements UserServices {
     RestTemplate restTemplate;
 
     @Autowired
-    HotelService hotelService;
-
-    @Autowired
-    RatingService ratingService;
+    ApiGatewayService apiGatewayService;
 
     //private Logger logger= LoggerFactory.getLogger(UserServicesImpl.class);
 
@@ -69,10 +60,10 @@ public class UserServicesImpl implements UserServices {
     }
 
     private List<Rating> getRatingsAndHotelOfUser(User user) {
-        List<Rating> ratings=ratingService.getRatingsOfUser(user.getId()).getBody();
+        List<Rating> ratings= apiGatewayService.getRatingsOfUser(user.getId()).getBody();
         assert ratings !=null;
         return ratings.stream().map(rating -> {
-            Hotel hotel=hotelService.getHotel(rating.getHotelId());
+            Hotel hotel=apiGatewayService.getHotel(rating.getHotelId());
             //Hotel hotel=restTemplate.getForObject("http://HOTEL-SERVICE/hotels/"+rating.getHotelId(), Hotel.class);
             rating.setHotel(hotel);
             return rating;
